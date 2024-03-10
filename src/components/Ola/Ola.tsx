f2a8f09fb8de7a72f1edb8d17167fe606289a5cd
Gabriel Alves Carvalho <gabriel_alvescarvalho@hotmail.com>
2024-03-10 17:57:47 -0300

    Contador Like

---
+++ src/components/Ola/Ola.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+interface OlaProps {
+  nomeProps: string;
+}
+
+const Ola: React.FC<OlaProps> = ({ nomeProps }) => {
+  const [contadorLikes, setContadorLikes] = useState<number>(0);
+
+  const incrementarContador = () => setContadorLikes(contadorLikes + 1);
+  const decrementarContador = () => setContadorLikes(contadorLikes - 1);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.greeting}>
+        Olá {nomeProps}, seu total de Likes é {contadorLikes}
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <Button title="Like" onPress={incrementarContador} color="#337eab" />
+        <Button title="Deslike" onPress={decrementarContador} color="#dc143c" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greeting: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 16,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    gap:5
+  },
+});
+
+export default Ola;
